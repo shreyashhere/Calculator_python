420c10f33494ecd124a3be0a0caa10dd56f7a2ef
Shreyash Dinraj Chavan <107064362+shreyashhere@users.noreply.github.com>
2024-01-14 18:46:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+import Arts
+import Operations
+
+print(Arts.calculator_ascii_art)
+print(" ---- welcome to calculator ---- ")
+base_amount = float(input(" please enter number 1 : "))
+Operations.calculate_the_answer(base_amount)
